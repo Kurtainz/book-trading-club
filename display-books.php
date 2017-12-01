3294c0a7752207909de5cef3351761879d42c9ba
Kurt Corbett <kurtainz@googlemail.com>
2017-12-01 15:54:52 +0000

    Buttons now behave as expected

--- display-books.php
+++ display-books.php
@@ -13,7 +13,7 @@
 	$query = "SELECT * FROM books";
 	$result = mysqli_fetch_all(makeQuery($db, $query));
 
-	$bookArr = [
+	$book_arr = [
 		'num_of_active_trades' => 0,
 		'num_of_trade_requests' => 0,
 		'trade_requests' => '',
@@ -22,46 +22,63 @@
 
 	// Loop through all books to populate trade request data
 	foreach ($result as $book => $arr) {
+		// String will indicate what kind of loan or request if in progress
+		$request_text = '';
+		if ($result[$book][1] === $_SESSION['id']) {
+			if (!is_null($result[$book][5])) {
+				$request_text = 'Book has been requested by another user';
+			}
+			elseif (!is_null($result[$book][6])) {
+				$request_text = 'You have loaned this book out';
+			}
+		}
+		elseif ($result[$book][5] === $_SESSION['id']) {
+			$request_text = 'You have requested this book';
+		}
+		elseif ($result[$book][6] === $_SESSION['id']) {
+			$request_text = 'You are currently borrowing this book';
+		}
 		$new_book_elements = "
 			<div data-isbn='{$result[$book][0]}' data-owner='{$result[$book][1]}' data-requestedBy='{$result[$book][5]}' class='col-md-3'>
 				<h2 class='book-title'>{$result[$book][2]}</h2>
 				<img src='{$result[$book][4]}'></img>
+				<p class='request-text'>$request_text</p>
 				<p>{$result[$book][3]}</p>
 				<a class='fa fa-check confirm'></i></a>
 				<a class='fa fa-times cancel'></a>
 			</div>
 		";
+		// If users owns this book and has been requested or if user has requested it
 		if (($result[$book][1] === $_SESSION['id'] && !is_null($result[$book][5])) || $result[$book][5] === $_SESSION['id']) {
-			$bookArr['num_of_trade_requests'] += 1;
-			$bookArr['trade_requests'] .= $new_book_elements;
+			$book_arr['num_of_trade_requests'] += 1;
+			$book_arr['trade_requests'] .= $new_book_elements;
 		}
+		// If user owns book and is already loaned out or book is currently loaned to user
 		elseif (($result[$book][1] === $_SESSION['id'] && !is_null($result[$book][6])) || $result[$book][6] === $_SESSION['id']) {
-			$bookArr['num_of_active_trades'] += 1;
-			$bookArr['active_trades'] .= $new_book_elements;
+			$book_arr['num_of_active_trades'] += 1;
+			$book_arr['active_trades'] .= $new_book_elements;
 		}
 	}
 
 	if (isset($_POST['update'])) {
-		$bookArr['type'] = 'update';
-		exit(json_encode($bookArr));
+		$book_arr['type'] = 'update';
+		exit(json_encode($book_arr));
 	}
 ?>
 
 <div class="container-fluid">
 	<h1><?php echo $title; ?></h1>
 	<div>
-		<button id="active-button" class="btn btn-secondary" href="">Active Trades <span id="active-trade-num"><?php echo $bookArr['num_of_active_trades']; ?></span></button>
-		<div id="active-trades"><?php echo $bookArr['active_trades']; ?></div>
-		<button id="trade-button" class="btn btn-success" href="">Trade Requests <span id="trade-request-num"><?php echo $bookArr['num_of_trade_requests']; ?></span></button>
+		<button id="active-button" class="btn btn-secondary" href="">Active Trades <span id="active-trade-num"><?php echo $book_arr['num_of_active_trades']; ?></span></button>
+		<div id="active-trades"><?php echo $book_arr['active_trades']; ?></div>
+		<button id="trade-button" class="btn btn-success" href="">Trade Requests <span id="trade-request-num"><?php echo $book_arr['num_of_trade_requests']; ?></span></button>
 		<div id="trade-requests">
-			<?php echo $bookArr['trade_requests']; ?>
+			<?php echo $book_arr['trade_requests']; ?>
 		</div>
 	</div>
 	<div>
 		<a class="btn btn-primary btn-lg" href="add-books.php">Add Books</a>
 	</div>
-
-	<button id="smoke">Smoke</button>
 
 	<div id="book-collection" class="row">
 
@@ -71,15 +88,11 @@
 				foreach ($result as $book => $arr) {
 					if ($title === "My Books") {
 						if ($result[$book][1] === $_SESSION['id']) {
-							$disabled = '';
-							if (!is_null($result[$book][6]) || !is_null($result[$book][5])) {
-								$disabled = 'disabled';
-							}
 							echo "<div class='col-md-3'>
 									<h2 class='book-title'>{$result[$book][2]}</h2>
 									<img src='{$result[$book][4]}'></img>
 									<p>{$result[$book][3]}</p>
-									<button $disabled data-isbn='{$result[$book][0]}' class='btn btn-danger delete-button'>Delete Book</button>
+									<button data-isbn='{$result[$book][0]}' class='btn btn-danger delete-button'>Delete Book</button>
 						  		  </div>
 						";
 						}
@@ -88,10 +101,6 @@
 						}
 					}
 					else {
-						$disabled = '';
-						if (!is_null($result[$book][6]) || !is_null($result[$book][5])) {
-							$disabled = 'disabled';
-						}
 						echo "<div class='col-md-3'>
 								<h2 class='book-title'>{$result[$book][2]}</h2>
 								<img src='{$result[$book][4]}'></img>
@@ -99,18 +108,15 @@
 						";
 						if ($result[$book][1] === $_SESSION['id']) {
 							echo "
-								<button $disabled data-isbn='{$result[$book][0]}' class='btn btn-danger delete-button'>Delete Book</button>
+								<button data-isbn='{$result[$book][0]}' class='btn btn-danger delete-button'>Delete Book</button>
 							  </div>
 							";
 						}
 						// Else add request book button
 						else {
 							$innerText = "Request Book";
-							if (!empty($disabled)) {
-								$innerText = "Requested";
-							}
 							echo "
-									<button $disabled data-isbn='{$result[$book][0]}' class='btn btn-success request-button'>$innerText</button>
+									<button data-isbn='{$result[$book][0]}' class='btn btn-success request-button'>$innerText</button>
 								</div>
 							";
 						}
--- scripts/handle-button-click.js
+++ scripts/handle-button-click.js
@@ -1,11 +1,14 @@
 window.onload = add_button_listeners();
 
+// Add listeners for all buttons
 function add_button_listeners() {
 	add_listener_to_trade_buttons();
 	add_listener_to_book_buttons();
 	add_listener_to_confirm_buttons();
+	change_buttons();
 }
 
+// Adds listeners to Active Trades and Trade Requests buttons at top of page
 function add_listener_to_trade_buttons() {
 	var trade_buttons = document.querySelectorAll('#active-button, #trade-button');
 
@@ -31,6 +34,7 @@ function add_listener_to_trade_buttons() {
 	});
 }
 
+// Adds listeners to buttons that appear under each book
 function add_listener_to_book_buttons() {
 	var book_buttons = document.querySelectorAll('.delete-button, .request-button');
 
@@ -52,6 +56,7 @@ function add_listener_to_book_buttons() {
 	});
 }
 
+// Adds listeners to confirm and cancel buttons in Active Trades and Trade Requests drop down divs
 function add_listener_to_confirm_buttons() {
 	var confirm_buttons = document.querySelectorAll('.confirm, .cancel');
 
@@ -102,7 +107,6 @@ function process_response(response) {
 			window.location.reload();
 			break;
 		case "requested":
-			change_button(response.isbn);
 			clear_divs();
 			break;
 		case "trade":
@@ -110,7 +114,6 @@ function process_response(response) {
 				console.log(response.error);
 			}
 			else {
-				change_button(response.isbn);
 				clear_divs();
 			}
 			break;
@@ -147,100 +150,31 @@ function update_divs(data) {
 	document.querySelector('#active-trade-num').innerHTML = active_trades.childElementCount;
 	document.querySelector('#trade-request-num').innerHTML = trade_requests.childElementCount;
 	add_listener_to_confirm_buttons();
+	change_buttons();
 }
 
-function change_button(isbn) {
-	console.log('Change Button');
-	var button = document.querySelector("button[data-isbn='" + isbn + "']");
-	if (button.classList.contains('request-button')) {
-		if (button.hasAttribute('disabled')) {
-			button.removeAttribute('disabled');
-			button.innerText = 'Request Book';
+// Adds or removes disabled properties on buttons and changes their text based on their trade condition
+function change_buttons() {
+	document.querySelectorAll('.request-button, .delete-button').forEach(function(button) {
+		var trade_request = document.querySelector('#trade-requests > div[data-isbn="' + button.dataset.isbn + '"]');
+		var active_trade = document.querySelector('#active-trades > div[data-isbn="' + button.dataset.isbn + '"]');
+		if (trade_request || active_trade) {
+			button.setAttribute('disabled', '');
+			if (trade_request) {
+				button.innerText = 'Trade Requested';
+			}
+			else if (active_trade) {
+				button.innerText = 'Loaning';
+			}
 		}
 		else {
-			button.setAttribute('disabled', '');
-			button.innerText = 'Requested';
-		}
-	}
-	else if (button.classList.contains('delete-button')) {
-		console.log('delete-button');
-		if (button.hasAttribute('disabled')) {
-			console.log('Undisabling');
 			button.removeAttribute('disabled');
-			button.innerText = 'Delete Book';
+			if (button.classList.contains('request-button')) {
+				button.innerText = 'Request Book';	
+			}
+			else if (button.classList.contains('delete-button')) {
+				button.innerText = 'Delete Book';
+			}
 		}
-		else {
-			console.log('Disabling');
-			button.setAttribute('disabled', '');
-			button.innerText = "Loaned Out";
-		}
-	}
+	});
 }
-
-
-// var trade_buttons = document.querySelectorAll('#active-button, #trade-button');
-
-// trade_buttons.forEach(function(button) {
-// 	button.addEventListener('click', function(e) {
-// 		if (e.target.id === 'active-button') {
-// 			var target_div = document.querySelector('#active-trades');
-// 			var other_div = document.querySelector('#trade-requests');
-// 		}
-// 		else {
-// 			var target_div = document.querySelector('#trade-requests');
-// 			var other_div = document.querySelector('#active-trades');
-// 		}
-// 		var display_style = window.getComputedStyle(target_div).display;
-// 		if (display_style === 'none') {
-// 			target_div.style.display = 'block'
-// 			other_div.style.display = 'none';
-// 		}
-// 		else {
-// 			target_div.style.display = 'none';
-// 		}
-// 	});
-// });
-
-// var confirm_buttons = document.querySelectorAll('.confirm, .cancel');
-
-// confirm_buttons.forEach(function(button) {
-// 	button.addEventListener('click', function(e) {
-// 		var props = {
-// 			isbn : e.target.parentNode.dataset.isbn,
-// 			owner_id : e.target.parentNode.dataset.owner,
-// 			requested_by : e.target.parentNode.dataset.requestedby,
-// 			trade_action : ''
-// 		}
-// 		if (e.target.classList.contains('confirm')) {
-// 			props.trade_action = 'confirm';
-// 		}
-// 		else {
-// 			props.trade_action = 'cancel';
-// 		}
-// 		var post_string = '';
-// 		for (prop in props) {
-// 			post_string += prop + "=" + props[prop] + "&";
-// 		}
-
-// 		make_XHR_request('trade.php', post_string);
-// 	});
-// });
-
-// var book_buttons = document.querySelectorAll('.delete-button, .request-button');
-
-// book_buttons.forEach(function(button) {
-// 	button.addEventListener('click', function(e) {
-// 		var isbn = e.target.dataset.isbn;
-// 		var destination = 'request-book.php';
-// 		// If button clicked is delete button
-// 		if (button.classList.contains('delete-button')) {
-// 			if (confirm("Are you sure you want to delete this book?")) {
-// 				destination = 'delete-book.php';
-// 			}
-// 			else {
-// 				return;
-// 			}
-// 		}
-// 		make_XHR_request(destination, 'isbn=' + isbn);
-// 	});
-// });
